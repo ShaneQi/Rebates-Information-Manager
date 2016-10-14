167ba6d57a557e6d5be6ba42fa18c6a34e7384be
shaneqi <qizengtai@gmail.com>
2016-10-14 13:19:11 -0500

    Add evaluation result output.

--- Evaluation/Asg2A-zxq150130/Asg2A-zxq150130/Form1.cs
+++ Evaluation/Asg2A-zxq150130/Asg2A-zxq150130/Form1.cs
@@ -42,25 +42,37 @@ namespace Asg2A_zxq150130
                 }
 
                 //  Calculate data and render them.
-                calculate();
+                
+                string result = calculate();
+                System.IO.StreamWriter sw = new System.IO.StreamWriter("./Evaluation-Result.txt");
+                sw.Write(result);
+                sw.Close();
             }
         }
 
-        private void calculate() {
+        private string calculate() {
+            string result = "";
+
             //  Number of records.
             numberLabel.Text = records.Count + " Records";
+            result += "Number of records: " + records.Count + " Records" + Environment.NewLine;
+
             //  Entering time.
             double[] enteringTimes = new double[records.Count];
             for (int i = 0; i < records.Count; i++) {
                 enteringTimes[i] = (records[i].createdTime - records[i].updatedTime).Duration().TotalSeconds;
             }
+            result += "Entering time: ";
             // Limiting double to 2 decimal places.
             double avg = Math.Truncate(enteringTimes.Average() * 100) / 100;
             double max = enteringTimes.Max();
             double min = enteringTimes.Min();
             timeLabelMax.Text = "max: " + max + " s";
+            result += "max: " + max + " s\t";
             timeLabelMin.Text = "min: " + min + " s";
+            result += "min: " + min + " s\t";
             timeLabelAvg.Text = "avg: " + avg + " s";
+            result += "avg: " + avg + " s" + Environment.NewLine;
 
             //  Entering interval.
             double[] intervalTimes = new double[records.Count - 1];
@@ -68,16 +80,23 @@ namespace Asg2A_zxq150130
             {
                 intervalTimes[i] = (records[i + 1].createdTime - records[i].updatedTime).Duration().TotalSeconds;
             }
+            result += "Entering interval: ";
             //  Limiting double to 2 decimal places.
             double iAvg = Math.Truncate(intervalTimes.Average() * 100) / 100;
             double iMax = intervalTimes.Max();
             double iMin = intervalTimes.Min();
             intervalLabelMax.Text = "max: " + iMax + " s";
+            result += "max: " + iMax + " s\t";
             intervalLabelMin.Text = "min: " + iMin + " s";
+            result += "min: " + iMin + " s\t";
             intervalLabelAvg.Text = "avg: " + iAvg + " s";
+            result += "avg: " + iAvg + " s" + Environment.NewLine;
 
             //  Total time.
-            totalLabel.Text = (records.Last().updatedTime - records.First().createdTime).Duration().TotalSeconds + " Seconds" ;
+            totalLabel.Text = (records.Last().updatedTime - records.First().createdTime).Duration().TotalSeconds + " Seconds";
+            result += "Total time: " + (records.Last().updatedTime - records.First().createdTime).Duration().TotalSeconds + " Seconds";
+
+            return result;
         }
     }
 }
